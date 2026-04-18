853ae3af0267481cc3b8ddcbb3487d68a28ff058
tyler <tylertruong124@gmail.com>
2026-04-17 20:31:11 -0700

    added data.js to store data, added detail panels for cards and edit data feature

---
+++ data.js
@@ -0,0 +1,105 @@
+const keyboards = [
+    {
+        brand: "Perry", 
+        model: "IU", 
+        layout: "80% WKL TKL",
+        case: "Aluminum",
+        weight: "Aluminum",
+        mount: "O-ring",
+        plate: "Polycarbonate",
+        switches: 
+        {
+            switchName: "Cherry MX Brown",
+            switchType: "Light-tactile",
+            actuationForce: "55g"
+        },
+        keycap: 
+        {
+            keycapManu: "GMK",
+            keycapName: "Modo Light"
+        },
+        description: "placeholder",
+        images: ["https://i.imgur.com/2F9HVSe.jpeg",
+                 "https://i.imgur.com/KDTGUbA.jpeg",
+                 "https://i.imgur.com/4qo2A8y.jpeg",
+                 "https://i.imgur.com/nm4d23U.jpeg",
+                 "https://images.squarespace-cdn.com/content/v1/667533f8c80fda5732ebbf73/68195383-67b3-44bb-9a47-27af84f0a7fe/D1316691.jpg?format=2500w"]
+    },
+
+    {
+        brand: "Matrix Lab", 
+        model: "8XV 3.0", 
+        layout: "80% WKL TKL",
+        case: "Aluminum",
+        weight: "Brass",
+        mount: "Gasket",
+        plate: "Polycarbonate",
+        switches:
+        {
+            switchName: "43 Studios OPBlack",
+            switchType: "Linear",
+            actuationForce: "60g"
+        },
+        keycap: 
+        {
+            keycapManu: "GMK",
+            keycapName: "Apollo"
+        },
+        description: "placeholder",
+        images: ["https://i.imgur.com/a5bCgRR.jpeg",
+                 "https://i.imgur.com/b1wXpyE.jpeg",
+                 "https://i.imgur.com/Zgd5gSO.jpeg",
+                 "https://i.imgur.com/QDGNblA.jpeg",
+                 "https://i.imgur.com/Z634V4I.jpeg",]
+    },
+
+    {
+        brand: "SingaKBD", 
+        model: "Kohaku R2", 
+        layout: "65%",
+        case: "Aluminum",
+        weight: "Brass",
+        mount: "Gasket",
+        plate: "Polycarbonate",
+        switches:
+        {
+            switchName: "43 Studios OPBlack",
+            switchType: "Linear",
+            actuationForce: "60g"
+        },
+        keycap: 
+        {
+            keycapManu: "GMK",
+            keycapName: "Apollo"
+        },
+        description: "placeholder",
+        images: ["https://images.squarespace-cdn.com/content/v1/667533f8c80fda5732ebbf73/5ff9e415-cbf4-4504-a3e3-382b44abddcd/2_63_4575.jpg?format=2500w",
+                 "https://images.squarespace-cdn.com/content/v1/667533f8c80fda5732ebbf73/169ee77c-611f-4f2d-bbfb-97b70bac8d5e/2_63_4530.jpg?format=2500w",
+                 "https://images.squarespace-cdn.com/content/v1/667533f8c80fda5732ebbf73/342884fa-8158-4ca7-8405-d4186c9ee4f6/2_63_4451.jpg?format=2500w",
+                 "https://images.squarespace-cdn.com/content/v1/667533f8c80fda5732ebbf73/f96e7bff-b0e5-4c04-9580-161271e0fe44/2_63_4479.jpg?format=2500w",
+                 "https://images.squarespace-cdn.com/content/v1/667533f8c80fda5732ebbf73/3f9d4ce9-c0ea-4c37-89e2-37501c411da6/2_63_4571.jpg?format=2500w"]
+    },
+
+    {
+        brand: "Realforce", 
+        model: "86U", 
+        layout: "80% TKL",
+        case: "Plastic",
+        weight: "N/A",
+        mount: "Top-mount",
+        plate: "Aluminum",
+        switches:
+        {
+            switchName: "Deskeys BKE Tactile",
+            switchType: "Topre",
+            actuationForce: "55g"
+        },
+        keycap: 
+        {
+            keycapManu: "Realforce",
+            keycapName: "Stock beige"
+        },
+        description: "placeholder",
+        images: []
+    },
+];
--- scripts.js
+++ scripts.js
@@ -1,85 +1,100 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
-
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
 
 // This function adds cards the page to display the data in the array
-function showCards() {
+let currentBoard = null;
+
+function showCards() 
+{
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
+  for (let i = 0; i < keyboards.length; i++) {
+    let board = keyboards[i];
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, board, i+1); // Edit title
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+
+
+function editCardContent(card, board, entryNum) 
+{
   card.style.display = "block";
+  card.addEventListener("click", function() {
+    openDetail(board);
+  });
 
   const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
+  cardHeader.textContent = entryNum + ". " + board.brand + " " + board.model;
 
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
+  const mainImage = card.querySelector(".main-image");
+  mainImage.src = board.images[0];
+  mainImage.alt = board.brand + " " + board.model;
 
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+  const thumbnailContainer = card.querySelector(".thumbnails");
+
+  for (let i = 0; i < board.images.length; i++)
+  {
+    const thumb = document.createElement("img");
+    thumb.src = board.images[i];
+    thumb.className = "thumbnail";
+
+    thumb.addEventListener("click", function() {
+      event.stopPropagation();
+      mainImage.src = board.images[i];
+    });
+
+    thumbnailContainer.appendChild(thumb);
+  }
+  console.log("new card:", board.brand);
 }
+
+
+
+function openDetail(board) 
+{
+  currentBoard = board;
+  const panel = document.getElementById("detail-panel");
+
+  document.getElementById("detail-board").textContent = board.brand + " " + board.model;   
+
+  const boardSpecs = document.getElementById("board-specs");  
+  boardSpecs.innerHTML = 
+  `
+    <li>Layout: ${board.layout}</li>
+    <li>Case Material: ${board.case}</li>
+    <li>Weight: ${board.weight}</li>
+    <li>Mount Style: ${board.mount}</li>
+    <li>Plate: ${board.plate}</li>
+    <li>Description: ${board.description}</li>
+  `
+  const switchSpecs = document.getElementById("switch-specs");
+  switchSpecs.innerHTML = 
+  `
+    <li>Name: ${board.switches.switchName}</li>
+    <li>Switch Type: ${board.switches.switchType}</li>
+    <li>Actuation Force: ${board.switches.actuationForce}</li>
+  `
+  const keycapSpecs = document.getElementById("keycap-specs");
+  keycapSpecs.innerHTML = 
+  `
+    <li>Manufacturer: ${board.keycap.keycapManu}</li>
+    <li>Name: ${board.keycap.keycapName}</li>
+  `
+
+  panel.style.display = "block";
+}
+
+
+
+function closeDetail() 
+{
+  document.getElementById("detail-panel").style.display = "none";
+}
+
+
 
 // This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
@@ -94,4 +109,34 @@ function quoteAlert() {
 function removeLastCard() {
   titles.pop(); // Remove last item in titles array
   showCards(); // Call showCards again to refresh
+}
+
+
+
+function openModal()
+{
+  console.log("openModal called, currentBoard is:", currentBoard);
+  const modal = document.getElementById("edit-modal")
+  modal.style.display = "block";
+}
+
+function closeModal() 
+{
+  document.getElementById("edit-modal").style.display = "none";
+}
+
+function saveEdit() 
+{
+  currentBoard.brand = document.getElementById("edit-brand").value;
+  currentBoard.model = document.getElementById("edit-model").value;
+  currentBoard.layout = document.getElementById("edit-layout").value;
+  currentBoard.case = document.getElementById("edit-case").value;
+  currentBoard.weight = document.getElementById("edit-weight").value;
+  currentBoard.mount = document.getElementById("edit-mount").value;
+  currentBoard.plate = document.getElementById("edit-plate").value;
+  currentBoard.description = document.getElementById("edit-description").value;
+
+  closeModal();
+  openDetail(currentBoard);
+  showCards();
 }
